7a74a6659694c41d3997983388de05f203a350e3
zaki tayyip <zakidayib1997@gmail.com>
2023-05-23 10:27:01 +0300

    fundamentals 1 assignment solved

---
+++ js-assignments.js
@@ -0,0 +1,53 @@
+
+
+
+// console.log('9' - '5'); //4
+// console.log('19' - '13' + '17'); //617
+// console.log('19' - '13' + 17); //add
+// console.log('123' < 57); //false
+// console.log(5 + 6 + '4' + 9 - 4 - 2); //1143
+
+// switch statement
+
+/*
+ Use a switch statement to log the following string for the given 'language':
+chinese or mandarin: 'MOST number of native speakers!'
+spanish: '2nd place in number of native speakers'
+english: '3rd place'
+hindi: 'Number 4'
+arabic: '5th most spoken language'
+for all other simply log 'Great language too :D'
+*/
+
+// let language = "hindi";
+// switch(language){
+//     case 'chinese':
+//         console.log(`MOST number of native speakers!`);
+//         break;
+//     case 'english':
+//         console.log(`3rd place`);
+//         break;
+
+//     case 'spanish':
+//         console.log(`2nd place in number of native speakers`);
+//         break;
+//     case 'hindi':
+//         console.log(`Number 4`);
+//         break;
+
+//     case '':
+//         console.log(`5th most spoken language`);
+//         break;
+//     default:
+//         console.log(`Great language too :D`);
+
+
+// }
+
+
+// The Conditional (Ternary) Operator
+
+let population = 50;
+
+population > 33 ? console.log(`Portugal's population is above average`):
+console.log(`Portugal's population is below average`);
